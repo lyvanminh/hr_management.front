fa5f4c878381ed18ce90a18a66da22e55e5b2054
lyvanminh <lyvanminh1002@gmail.com>
2021-11-01 10:53:20 +0700

    add upload candidate and send request evaluates

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,19 @@ import user from './modules/user';
 import auth from './modules/auth';
 import message from './modules/message';
 // import ads from './modules/ads';
+import state from "./state";
+import * as getters from './getters';
+import * as mutations from "./mutations";
+import * as actions from "./actions";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions,
+
     modules: {
         loading,
         auth,
